a72f5ff285010999dbbeec0ae0484825495ad58a
JamesVictor-O <victorjames40v@gmail.com>
2024-12-21 01:16:00 +0100

    learnt about arrays

--- hello.sol
+++ hello.sol
@@ -14,11 +14,42 @@ contract StateVariables{
     //      myState=_text;
     //      myNum=_no;
     //  }
+     uint public maxNum=type(uint).max;
+     int public  maxIntNum=type(int).max;
 
     function update(string memory _text, uint _no) public {
          myState=_text;
          myNum=_no;
     }
+    
+    // array
+     
+    //  array of integers
+    uint[] public uintArray=[1,2,3];
 
-    // myNumber number=[1,3,4];
+    // array of strings
+    string[] public stringArray=['james','victor', 'ochula'];
+
+    // array
+    string[] public emptyStirngArray;
+
+    function addValue(string memory _value) public {
+        emptyStirngArray.push(_value);
+    }
+
+    function arrayCount()public view returns (uint[] memory){
+        return uintArray;
+    }
+    function arrayString() public view  returns (string[] memory){
+          return stringArray;
+    }
+
+    // 2D arrar, multidimentional array
+
+    uint[][] public array2D=[[1,2,3],[4,5,6]];
+
+    function print2D() public view  returns (uint[][] memory){
+        return  array2D;
+    }
+
 } 
