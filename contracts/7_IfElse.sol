3b611727dd73b8f98b2a22c2a1bc8f456db6d2b1
JamesVictor-O <victorjames40v@gmail.com>
2024-12-25 08:30:55 +0100

    learnt about loops

--- contracts/7_IfElse.sol
+++ contracts/7_IfElse.sol
@@ -3,16 +3,27 @@ pragma solidity ^0.8.13;
 
 contract IfElse{
     uint256 public myNum=5;
+    uint256 public nftCollection=0;
+    uint[] public  data;
     string public  myString;
+    bool public result;
     function get(uint256 _num) public returns (string memory) {
-        //  if (_num == 5){
-        //     myString = "Hey the value of my Num is 5";
-        //  } else if (_num == 4){
-        //     myString = "Hey the value of myNum is 4";
-        //  }else {
-        //     myString="Hey dalatue is not equals to 4 or 5";
-        //  }
-
+       
         return _num == 5 ? myString = "Hey the value of my Num is 5" :  myString = "Hey not 5 ";
+    }
+
+    function checkOverFlow(uint256 _num1, uint _num2) public {
+        uint256 sum =_num1 + _num2;
+        assert((sum <= 255));
+        result=true;
+
+    }
+
+    function whileLoop() public returns (uint[] memory){
+         while(nftCollection < 5){
+            nftCollection ++;
+            data.push(nftCollection);
+         }
+         return  data;
     }
 }
